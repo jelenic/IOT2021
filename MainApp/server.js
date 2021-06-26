c0ee156781db840ba53baf367e78c62d39462539
jelenic <astro.jakov@gmail.com>
2021-06-26 17:52:26 +0200

    clean, seperate front and back to different docker containers

--- MainApp/server.js
+++ MainApp/server.js
@@ -1,12 +1,8 @@
 const express = require('express');
-//const bodyParser = require('body-parser');
-//require('dotenv').config();
 const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 const port = process.env.SERVER_PORT /*|| 4000*/;
-//const dbURI = 'mongodb+srv://readWrite:<rKsnW2pPLafbHHz>@nodeloraapp.rguzt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const {json, urlencoded} = express;
 
 
@@ -19,40 +15,8 @@ app.use(json());
 app.use(urlencoded({extended: false}));
 app.use('/api/', routes);
 
-/*app.use('/',(req,res)=>{
-    res.send('test')
-})*/
-
-app.get('/testJWT', authenticateToken, (req, res) => {
-    res.send('testJWT');
-})
-
 
 app.listen(port, () => {
     console.info('server started on port: ' + port);
     //console.log(process.env.DBLINK);
 })
-
-//import this function from authService imas kopiju funkcije rip 6:00 kodiranje   min 15
-function authenticateToken(req, res, next){
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null){
-        return 401;
-    }
-
-    jwt.verify(token, 'nestostabitrebaocitkljuc', (err,user) => {
-        console.log('inside verifyJWT');
-        if (err){
-            return 403;
-        }
-        else{
-            req.user = user;
-            next();
-        }
-    })
-}
-
-
-//connection with mongodb via user readWrite    pass: rKsnW2pPLafbHHz
-// mongodb link     mongodb+srv://readWrite:<password>@nodeloraapp.rguzt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
--- WSSApp/server.js
+++ WSSApp/server.js
@@ -7,59 +7,44 @@ const mongoose = require('mongoose');
 const elsysDataDecoder = require('./services/elsysDataDecoder');
 const oxobuttonDecoder = require('./services/oxobuttonDecoder');
 
-const port = /*process.env.SERVER_PORT ||*/ 3000;
-const dbURI = 'mongodb+srv://readWrite:rKsnW2pPLafbHHz@nodeloraapp.rguzt.mongodb.net/diplomskiRadJelenic?retryWrites=true&w=majority';
-const wsURL = 'wss://eu1.loriot.io/app?token=vnoh3wAAAA1ldTEubG9yaW90LmlvCoH9Jt8pL4NSI5MpfkWUjQ==';
+const dotenv = require('dotenv');
+dotenv.config();
 
-// http://oxobutton.ch/products/oxobutton-lorawan/documentation decoder for oxobutton
+const port = process.env.SERVER_PORT_W;
+//const dbURI = 'mongodb+srv://readWrite:rKsnW2pPLafbHHz@nodeloraapp.rguzt.mongodb.net/diplomskiRadJelenic?retryWrites=true&w=majority';
+//const wsURL = 'wss://eu1.loriot.io/app?token=vnoh3wAAAA1ldTEubG9yaW90LmlvCoH9Jt8pL4NSI5MpfkWUjQ==';
+const dbURI = process.env.DBLINK;
+const wsURL = process.env.WSURL;
+
 
 const app = express();
 app.listen(port, () => {
     console.info('writeToDB server started on port: ' + port);
     //let connectedToDB = false;
     //connectToDB();
+    webSocketC();
 })
 
-app.get('/', function (req, res) {
-    /*console.log('data')
-    console.log('3008000000006419015afed513e7')
-    msg = oxobuttonDecoder.Decoder(oxobuttonDecoder.hexToBytes('3008000000006419015afed513e7'));
-    console.log('msg')
-    console.log(msg)*/
-    /*mongoose.connect(dbURI, 
-        { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, 
-        async function(err, db){
-    const uplinkMessageM = new UplinkMessage({
-        cmd: "rx",
-        EUI: "test",
-        ts: 0,
-        bat: 0,
-        fcnt: 0,
-        port: 0,
-        ack: false,
-        data: "ayyyyyLmao"
+function connectToDB(){
+    mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+        console.log('connected to database');
+        connectedToDB = true;
+    }).catch((err)=>{
+        console.error(`error connecting to db. \n${err}`);
     })
-    uplinkMessageM.save()
-    .then((result) => {
-        console.log('saved measurement to db:' + result);
-        mongoose.connection.close();
-    }).catch((err) => {
-        console.log(uplinkMessageM);
-        console.log('something went wrong');
-        console.log(err);
-        mongoose.connection.close();
-    });
-})*/
+}
+
+function webSocketC(){
     const connection = new webSocket(wsURL);
 
     connection.onopen = () => {
         console.log("Loriot connection established.");
-        res.sendStatus(200);
+        //res.sendStatus(200);
     }
 
     connection.onerror = error => {
         console.log(`WebSocket error: ${error}`);
-        res.send(error);
+        //res.send(error);
     }
 
     connection.onmessage = e => {
@@ -81,24 +66,6 @@ app.get('/', function (req, res) {
                 reject(err);
             });
             if ((typeof jdata.data!=="undefined" || jdata.data!=="efff") && jdata.cmd==="rx"){
-                /*const uplinkMessageM = new UplinkMessage({
-                    cmd: jdata.cmd,
-                    EUI: jdata.EUI,
-                    ts: jdata.ts,
-                    bat: jdata.bat,
-                    fcnt: jdata.fcnt,
-                    port: jdata.port,
-                    ack: jdata.ack,
-                    data: jdata.data
-                })
-                uplinkMessageM.save()
-                .then((result) => {
-                    console.log('saved measurement to db uplinkMessage:' + result);
-                }).catch((err) => {
-                    console.log(uplinkMessageM);
-                    console.log('something went wrong uplinkMessage');
-                    console.log(err);
-                });*/
                 if (existingSensor == null){
                     console.log('sensor does not exist' )
                 }
@@ -140,76 +107,9 @@ app.get('/', function (req, res) {
                     });
                 }
             }
-            /*    splitData = e.data.split(os.EOL);
-                splitData.forEach(element => {
-                    json = JSON.parse(element);
-                    if ((typeof json['data']!=="undefined" || json['data']!=="efff") && json['cmd']==="rx"){
-                        //const msg = {msg:json['data']}
-                        const uplinkMessageM = new UplinkMessage({
-                            cmd: json['cmd'],
-                            EUI: json['EUI'],
-                            ts: json['ts'],
-                            bat: json['bat'],
-                            fcnt: json['fcnt'],
-                            port: json['port'],
-                            ack: json['ack'],
-                            data: json['data']
-                        })
-                        uplinkMessageM.save()
-                        .then((result) => {
-                            console.log('saved measurement to db:' + result);
-                            //mongoose.connection.close();
-                        }).catch((err) => {
-                            console.log(uplinkMessageM);
-                            console.log('something went wrong');
-                            console.log(err);
-                            //mongoose.connection.close();
-                        });
-                        if (existingSensor == null){
-                            console.log('sensor does not exist' )
-                        }
-                        else{
-                            console.log('type:' + existingSensor.type);
-                            if (exsistingSensor.type == 'elsys'){
-                                const msg = elsysDataDecoder.decode(elsysDataDecoder.hexToBytes(json['data']));
-                            }
-                            else{
-                                msg = 'default'
-                            }
-                            const uplinkMessage2M = new UplinkMessage({
-                                cmd: json['cmd'],
-                                EUI: json['EUI'],
-                                ts: json['ts'],
-                                bat: json['bat'],
-                                fcnt: json['fcnt'],
-                                port: json['port'],
-                                ack: json['ack'],
-                                data: msg
-                            },{collection : json['EUI']})
-                            uplinkMessage2M.save()
-                            .then((result) => {
-                                console.log('saved measurement to db:' + result);
-                                mongoose.connection.close();
-                            }).catch((err) => {
-                                console.log(uplinkMessageM);
-                                console.log('something went wrong');
-                                console.log(err);
-                                mongoose.connection.close();
-                            });
-                        }
-                    }
-                })*/
                 
         })
         
         //save data to db
     }
-});
-function connectToDB(){
-    mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-        console.log('connected to database');
-        connectedToDB = true;
-    }).catch((err)=>{
-        console.error(`error connecting to db. \n${err}`);
-    })
 }
